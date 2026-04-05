92a0ebeda64f4f6ed0701ac8c7b1442d7015082e
ayiekoderrick-8068 <ayiekoderrick@gmail.com>
2026-04-05 22:21:15 +0300

    Add function implementations and fix test script

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
 
+function isPalindrome(word) {
+    let lowerWord = word.toLowerCase();
+    return lowerWord === lowerWord.split('').reverse().join('');
+}
 
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * (discountPercentage / 100));
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
